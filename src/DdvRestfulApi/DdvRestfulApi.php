d7bf796ac3aa191d2fbe1863ef896f99ac02c694
hua <yuchonghua@163.com>
2017-04-25 21:16:57 +0800

    add createGuid

--- src/DdvRestfulApi/DdvRestfulApi.php
+++ src/DdvRestfulApi/DdvRestfulApi.php
@@ -130,5 +130,11 @@
       return $header ;
     }
 
-
+    //生成guid
+    public function createGuid() {
+      $charid = strtolower(md5(uniqid(mt_rand(), true)));
+      $hyphen = chr(45);// "-"
+      $uuid = substr($charid, 0, 8).$hyphen.substr($charid, 8, 4).$hyphen.substr($charid,12, 4).$hyphen.substr($charid,16, 4).$hyphen.substr($charid,20,12);
+      return $uuid;
+    }
   }
