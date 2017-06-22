c005cefad386c358505737d17794da54b2b8c94c
曾繁耀 <zengfanyao@han-zi.cn>
2017-06-23 02:30:51 +0800

    fixbug

--- src/DdvRestfulApi/Auth/AuthSignDdvUrlV1.php
+++ src/DdvRestfulApi/Auth/AuthSignDdvUrlV1.php
@@ -6,8 +6,6 @@ use \DdvPhp\DdvRestfulApi\Exception\AuthError as AuthErrorException;
 */
 class AuthSignDdvUrlV1 extends AuthAbstract
 {
-  private $regAuth = 
-    '/^([\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12})\/([0-9a-zA-Z,-]+)\/([\da-f]{4}-[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}-[\da-f]{8})\/([\d]{4}-[\d]{2}-[\d]{2}T[\d]{2}:[\d]{2}:[\d]{2}Z)\/(\d+)\/([\w\-\;]+)\/([\da-f]{64})$/i';
   protected function sign()
   {
     // 试图旧授权信息
@@ -311,4 +309,3 @@ class AuthSignDdvUrlV1 extends AuthAbstract
     return http_build_query($queryData);
   }
 }
- ?>
