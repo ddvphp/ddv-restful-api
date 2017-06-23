ee3bc33dbeb1db094f15129f0fff22221e5a23e4
曾繁耀 <zengfanyao@han-zi.cn>
2017-06-23 15:35:12 +0800

    fixbug

--- src/DdvRestfulApi/Exception/RJsonError.php
+++ src/DdvRestfulApi/Exception/RJsonError.php
@@ -5,7 +5,7 @@ namespace DdvPhp\DdvRestfulApi\Exception;
 class RJsonError extends \DdvPhp\DdvException\Error
 {
   // 魔术方法
-  public function __construct( $message = 'Unknown Error', $errorId = 'UNKNOWN_ERROR' , $code = '400', $errorData )
+  public function __construct( $message = 'Unknown Error', $errorId = 'UNKNOWN_ERROR' , $code = '400', $errorData  = array() )
   {
     parent::__construct( $message , $errorId , $code, $errorData );
   }
