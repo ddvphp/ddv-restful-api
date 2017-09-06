39d59dbcb2d017f0cd2b7ba48328340b76b6f670
hua <yuchonghua@163.com>
2017-09-06 15:15:48 +0800

    AuthSignDdvUrlV1 add $_GET['redirect_uri']

--- src/DdvRestfulApi/Auth/AuthSignDdvUrlV1.php
+++ src/DdvRestfulApi/Auth/AuthSignDdvUrlV1.php
@@ -30,147 +30,155 @@ class AuthSignDdvUrlV1 extends AuthAbstract
   }
   private function checkAuth()
   {
-    if (empty($this->authorization)) {
-      throw new AuthErrorException('Authentication Info Length Error','AUTHORIZATION_ERROR_INFO_LENGTH',403);
-    }
     try {
-      $authorization = preg_replace('/\_/', '/', preg_replace('/\-/', '+', $this->authorization));
-    } catch (Exception $e) {
-      throw new AuthErrorException('Authentication Parse Error','AUTHENTICATION_PARSE_ERROR',403);
-    }
-    try {
-      $authorization = base64_decode($authorization);
-    } catch (Exception $e) {
-      throw new AuthErrorException('Authentication Base64 Decode Error','AUTHENTICATION_BASE64_DECODE_ERROR',403);
-    }
-    try {
-      $clientSign = substr($authorization, -16);
-      if (empty($clientSign)) {
+      if (empty($this->authorization)) {
+        throw new AuthErrorException('Authentication Info Length Error','AUTHORIZATION_ERROR_INFO_LENGTH',403);
+      }
+      try {
+        $authorization = preg_replace('/\_/', '/', preg_replace('/\-/', '+', $this->authorization));
+      } catch (Exception $e) {
+        throw new AuthErrorException('Authentication Parse Error','AUTHENTICATION_PARSE_ERROR',403);
+      }
+      try {
+        $authorization = base64_decode($authorization);
+      } catch (Exception $e) {
+        throw new AuthErrorException('Authentication Base64 Decode Error','AUTHENTICATION_BASE64_DECODE_ERROR',403);
+      }
+      try {
+        $clientSign = substr($authorization, -16);
+        if (empty($clientSign)) {
+          throw new AuthErrorException('Authentication client sign must input','AUTHENTICATION_CLIENT_SIGN_MUST_INPUT',403);
+        }
+      } catch (Exception $e) {
         throw new AuthErrorException('Authentication client sign must input','AUTHENTICATION_CLIENT_SIGN_MUST_INPUT',403);
       }
-    } catch (Exception $e) {
-      throw new AuthErrorException('Authentication client sign must input','AUTHENTICATION_CLIENT_SIGN_MUST_INPUT',403);
-    }
-    try {
-      $authorization = substr($authorization, 0, -16);
-    } catch (Exception $e) {
-      throw new AuthErrorException('Authentication wrong format as content','AUTHORIZATION_ERROR_FORMAT_WRONG',403);
-    }
-    if (empty($authorization)) {
-      throw new AuthErrorException('Authentication Info Length Error','AUTHORIZATION_ERROR_INFO_LENGTH',403);
-    }
-    try {
-      $authorization = gzuncompress($authorization);
-    } catch (Exception $e) {
-      throw new AuthErrorException('Authentication gzuncompress error','AUTHENTICATION_GZUNCOMPRESS_ERROR',403);
-    }
-    try {
-      $authorization = json_decode($authorization, true);
-    } catch (Exception $e) {
-      throw new AuthErrorException('Authentication json decode error','AUTHENTICATION_JSON_DECODE_ERROR',403);
-    }
-    $noSignQuery = empty($authorization[3])?array():$authorization[3];
-    $noSignQuery = ($noSignQuery===true)||is_array($authorization[3])?$authorization[3]:array();
-    $headersKeys = (!empty($authorization[4]))&&is_array($authorization[4])?$authorization[4]:array();
+      try {
+        $authorization = substr($authorization, 0, -16);
+      } catch (Exception $e) {
+        throw new AuthErrorException('Authentication wrong format as content','AUTHORIZATION_ERROR_FORMAT_WRONG',403);
+      }
+      if (empty($authorization)) {
+        throw new AuthErrorException('Authentication Info Length Error','AUTHORIZATION_ERROR_INFO_LENGTH',403);
+      }
+      try {
+        $authorization = gzuncompress($authorization);
+      } catch (Exception $e) {
+        throw new AuthErrorException('Authentication gzuncompress error','AUTHENTICATION_GZUNCOMPRESS_ERROR',403);
+      }
+      try {
+        $authorization = json_decode($authorization, true);
+      } catch (Exception $e) {
+        throw new AuthErrorException('Authentication json decode error','AUTHENTICATION_JSON_DECODE_ERROR',403);
+      }
+      $noSignQuery = empty($authorization[3])?array():$authorization[3];
+      $noSignQuery = ($noSignQuery===true)||is_array($authorization[3])?$authorization[3]:array();
+      $headersKeys = (!empty($authorization[4]))&&is_array($authorization[4])?$authorization[4]:array();
 
-    try {
-      @list($sessionId, $signTime, $expiredTimeOffset) = $authorization;
-    } catch (Exception $e) {
-      throw new AuthErrorException('Authentication json decode error','AUTHENTICATION_JSON_DECODE_ERROR',403);
-    }
-    if (empty($sessionId)) {
-      throw new AuthErrorException('Authentication sessionId Error','AUTHENTICATION_SESSIONID_ERROR',403);
-    }
+      try {
+        @list($sessionId, $signTime, $expiredTimeOffset) = $authorization;
+      } catch (Exception $e) {
+        throw new AuthErrorException('Authentication json decode error','AUTHENTICATION_JSON_DECODE_ERROR',403);
+      }
+      if (empty($sessionId)) {
+        throw new AuthErrorException('Authentication sessionId Error','AUTHENTICATION_SESSIONID_ERROR',403);
+      }
 
-    // 授权数据
-    $data = $this->getAuthData($sessionId);
+      // 授权数据
+      $data = $this->getAuthData($sessionId);
 
-    if (empty($data)||empty($data['card'])||empty($data['key'])) {
-      throw new AuthErrorException('Authentication auth data empty','AUTHENTICATION_AUTH_DATA_EMPTY',403);
-    }
-    $sessionCard = $data['card'];
+      if (empty($data)||empty($data['card'])||empty($data['key'])) {
+        throw new AuthErrorException('Authentication auth data empty','AUTHENTICATION_AUTH_DATA_EMPTY',403);
+      }
+      $sessionCard = $data['card'];
 
-    // 通过
-    $signHeaders = $this->getSignHeaders($headersKeys);
-    // 授权字符串
-    $authString = "{$this->authVersion}/{$sessionId}/{$sessionCard}/{$signTime}/{$expiredTimeOffset}";
-    //生成加密key
-    $signingKey = hash_hmac('sha256', $authString, $data['key']);
+      // 通过
+      $signHeaders = $this->getSignHeaders($headersKeys);
+      // 授权字符串
+      $authString = "{$this->authVersion}/{$sessionId}/{$sessionCard}/{$signTime}/{$expiredTimeOffset}";
+      //生成加密key
+      $signingKey = hash_hmac('sha256', $authString, $data['key']);
 
-    //获取请求的uri
-    $canonicalUri = isset($_SERVER['REQUEST_URI'])?$_SERVER['REQUEST_URI']:'';
-    //去除//
-    $canonicalUri = substr($canonicalUri, 0, 2)==='//'?substr($canonicalUri, 1):$canonicalUri;
-    $canonicalUris = DdvUrl::parse($canonicalUri);
-    $canonicalPath = isset($canonicalUris['path'])?$canonicalUris['path']:'';
-    //取得query
-    $canonicalQuery = isset($canonicalUris['query'])?$canonicalUris['query']:'';
-    //取得path
-    $canonicalPath = substr($canonicalPath, 0, 1)==='/'?$canonicalPath:('/'.$canonicalPath);
-    $canonicalPath = DdvUrl::urlEncodeExceptSlash($canonicalPath);
+      //获取请求的uri
+      $canonicalUri = isset($_SERVER['REQUEST_URI'])?$_SERVER['REQUEST_URI']:'';
+      //去除//
+      $canonicalUri = substr($canonicalUri, 0, 2)==='//'?substr($canonicalUri, 1):$canonicalUri;
+      $canonicalUris = DdvUrl::parse($canonicalUri);
+      $canonicalPath = isset($canonicalUris['path'])?$canonicalUris['path']:'';
+      //取得query
+      $canonicalQuery = isset($canonicalUris['query'])?$canonicalUris['query']:'';
+      //取得path
+      $canonicalPath = substr($canonicalPath, 0, 1)==='/'?$canonicalPath:('/'.$canonicalPath);
+      $canonicalPath = DdvUrl::urlEncodeExceptSlash($canonicalPath);
 
-    if ($noSignQuery === true){
-        $canonicalQuery = '';
-    }else {
-      // 重新排序编码
-      $canonicalQuery = Sign::canonicalQuerySort($canonicalQuery);
-      if ($canonicalQuery) {
-        $tPrefix = $this->signBaseHeadersPrefix;
-        $authPrefixs = array(
-          strtolower($tPrefix . 'auth'),
-          strtolower($tPrefix . 'authorization'),
-        );
-        $canonicalQueryArray = explode('&', $canonicalQuery);
-        $canonicalQueryArrayNew = array();
-        foreach ($canonicalQueryArray as $key => $t) {
-          $ts = explode('=', $t);
-          if (!($ts && $ts[0] && in_array(strtolower($ts[0]), $authPrefixs))) {
-            if (!in_array($ts[0], $noSignQuery)) {
-              $canonicalQueryArrayNew[] = $t;
+      if ($noSignQuery === true){
+          $canonicalQuery = '';
+      }else {
+        // 重新排序编码
+        $canonicalQuery = Sign::canonicalQuerySort($canonicalQuery);
+        if ($canonicalQuery) {
+          $tPrefix = $this->signBaseHeadersPrefix;
+          $authPrefixs = array(
+            strtolower($tPrefix . 'auth'),
+            strtolower($tPrefix . 'authorization'),
+          );
+          $canonicalQueryArray = explode('&', $canonicalQuery);
+          $canonicalQueryArrayNew = array();
+          foreach ($canonicalQueryArray as $key => $t) {
+            $ts = explode('=', $t);
+            if (!($ts && $ts[0] && in_array(strtolower($ts[0]), $authPrefixs))) {
+              if (!in_array($ts[0], $noSignQuery)) {
+                $canonicalQueryArrayNew[] = $t;
+              }
             }
           }
+          $canonicalQuery = implode('&', $canonicalQueryArrayNew);
         }
-        $canonicalQuery = implode('&', $canonicalQueryArrayNew);
+      }
+
+      // 获取签名头
+      $canonicalHeaders = Sign::getCanonicalHeaders($signHeaders);
+      //生成需要签名的信息体
+      $canonicalRequest = "{$this->method}\n{$canonicalPath}\n{$canonicalQuery}\n{$canonicalHeaders}";
+
+      if (empty($noSignQuery)) {
+        $canonicalRequest.="\n";
+      }elseif($noSignQuery===true){
+        $canonicalRequest.="\ntrue";
+      }else{
+        $canonicalRequest.="\n".implode(',', $noSignQuery);
+      }
+      if (empty($headersKeys)) {
+        $canonicalRequest.="\n";
+      }else{
+        $canonicalRequest.="\n".implode(',', $headersKeys);
+      }
+
+      $signCheck = hash_hmac('md5', $canonicalRequest, $signingKey, true);
+
+      if ($clientSign !== $signCheck) {
+        $errorData = array('debugSign'=>array());
+        $errorData['debugSign']['canonicalRequest'] = $canonicalRequest;
+        $errorData['debugSign']['sessionId'] = $sessionId;
+        $errorData['debugSign']['sessionCard'] = $sessionCard;
+        $errorData['debugSign']['clientSign'] = $clientSign;
+        $errorData['debugSign']['signHeaderKeysStr'] = $headersKeys;
+        $errorData['debugSign']['serverSessionKey'] = $data['key'];
+        $errorData['debugSign']['signingKey'] = $signingKey;
+        $errorData['debugSign']['sessionSignCheck'] = $signCheck;
+        throw new AuthErrorException('Signature authentication failure', 'AUTHORIZATION_SIGNATURE_FAILURE', 403, $errorData);
+      }
+      self::$accessKeyId = $sessionId;
+      self::$sessionCard = $sessionCard;
+      $this->signInfo['sessionId'] = $sessionId;
+      return true;
+    } catch (AuthErrorException $e) {
+      if (empty($_GET['redirect_uri'])) {
+        throw $e;
+      }else{
+        @header('Location: '.$_GET['redirect_uri']);
+        die();
       }
     }
-
-    // 获取签名头
-    $canonicalHeaders = Sign::getCanonicalHeaders($signHeaders);
-    //生成需要签名的信息体
-    $canonicalRequest = "{$this->method}\n{$canonicalPath}\n{$canonicalQuery}\n{$canonicalHeaders}";
-
-    if (empty($noSignQuery)) {
-      $canonicalRequest.="\n";
-    }elseif($noSignQuery===true){
-      $canonicalRequest.="\ntrue";
-    }else{
-      $canonicalRequest.="\n".implode(',', $noSignQuery);
-    }
-    if (empty($headersKeys)) {
-      $canonicalRequest.="\n";
-    }else{
-      $canonicalRequest.="\n".implode(',', $headersKeys);
-    }
-
-    $signCheck = hash_hmac('md5', $canonicalRequest, $signingKey, true);
-
-    if ($clientSign !== $signCheck) {
-      $errorData = array('debugSign'=>array());
-      $errorData['debugSign']['canonicalRequest'] = $canonicalRequest;
-      $errorData['debugSign']['sessionId'] = $sessionId;
-      $errorData['debugSign']['sessionCard'] = $sessionCard;
-      $errorData['debugSign']['clientSign'] = $clientSign;
-      $errorData['debugSign']['signHeaderKeysStr'] = $headersKeys;
-      $errorData['debugSign']['serverSessionKey'] = $data['key'];
-      $errorData['debugSign']['signingKey'] = $signingKey;
-      $errorData['debugSign']['sessionSignCheck'] = $signCheck;
-      throw new AuthErrorException('Signature authentication failure', 'AUTHORIZATION_SIGNATURE_FAILURE', 403, $errorData);
-    }
-
-    self::$accessKeyId = $sessionId;
-    self::$sessionCard = $sessionCard;
-    $this->signInfo['sessionId'] = $sessionId;
-    return true;
   }
   public static function getSignUrl($sessionId, $authData, $path = '/', $query = array(), $noSignQuery = array(), $method = 'GET', $headers = array(), $authClassName = null)
   {
