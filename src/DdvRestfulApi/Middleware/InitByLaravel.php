ac762d5de06e75958acbd7a49f46bfeda5be80a5
桦 <yuchonghua@163.com>
2018-01-14 19:11:48 +0800

    setIsHandle

--- src/DdvRestfulApi/Middleware/InitByLaravel.php
+++ src/DdvRestfulApi/Middleware/InitByLaravel.php
@@ -10,6 +10,7 @@ use PhpParser\Error;
 class InitByLaravel
 {
   protected $isDdvRestfulApiInit = false;
+  protected $isHandle = true;
   /**
    * Handle an incoming request.
    *
@@ -18,8 +19,14 @@ class InitByLaravel
    * @param  string|null  $guard
    * @return mixed
    */
+  public static function setIsHandle($input){
+      InitByLaravel::$isHandle = (boolean)$input;
+  }
   public function handle($request, Closure $next, $guard = null)
   {
+      if (!InitByLaravel::$isHandle){
+          return $next($request);
+      }
     if(!$this->isDdvRestfulApiInit){
       $this->isDdvRestfulApiInit = true;
       $restfulApi = \DdvPhp\DdvRestfulApi::getInstance();
@@ -32,38 +39,40 @@ class InitByLaravel
     }
     $r = &$restfulApi->responseData;
     $response = $next($request);
-    if(class_exists('DdvPhp\DdvPage')){
-      // 分页
-      if(class_exists('Illuminate\Pagination\LengthAwarePaginator') && $response->original instanceof \Illuminate\Pagination\LengthAwarePaginator){
-        $response->original = new \DdvPhp\DdvPage($response->original);
-      }
-      // 如果是\DdvPhp\DdvPage
-      if($response->original instanceof \DdvPhp\DdvPage){
-        $response->original = $response->original->toArray();
-      }
-    }
-
-    $content = $response->getContent();
-    if(is_array($response->original)){
-      $r = array_merge($r , $response->original);
-    }else if(is_string($response->original)){
-      try{
-        $r = array_merge($r , json_decode(json_encode($response->original),true));
-      }catch(\Exception $e){
-        return $response;
-      }
-    }else{
-      try{
-        $r = array_merge($r , json_decode((string)$response->original,true));
-      }catch(\Exception $e){
-        try{
-          $r = array_merge($r , json_decode((string)$content,true));
-        }catch(\Exception $e){
-          if (!empty($response->original)) {
-            return  $response;
+    if (isset($response->original)){
+        if(class_exists('DdvPhp\DdvPage')){
+          // 分页
+          if(class_exists('Illuminate\Pagination\LengthAwarePaginator') && $response->original instanceof \Illuminate\Pagination\LengthAwarePaginator){
+            $response->original = new \DdvPhp\DdvPage($response->original);
+          }
+          // 如果是\DdvPhp\DdvPage
+          if($response->original instanceof \DdvPhp\DdvPage){
+            $response->original = $response->original->toArray();
           }
         }
-      }
+
+        $content = $response->getContent();
+        if(is_array($response->original)){
+          $r = array_merge($r , $response->original);
+        }else if(is_string($response->original)){
+          try{
+            $r = array_merge($r , json_decode(json_encode($response->original),true));
+          }catch(\Exception $e){
+            return $response;
+          }
+        }else{
+          try{
+            $r = array_merge($r , json_decode((string)$response->original,true));
+          }catch(\Exception $e){
+            try{
+              $r = array_merge($r , json_decode((string)$content,true));
+            }catch(\Exception $e){
+              if (!empty($response->original)) {
+                return  $response;
+              }
+            }
+          }
+        }
     }
     if (empty($r)){
       return $response;
